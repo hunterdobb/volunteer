28eb99953de4dddc1a2992ec944d9e39e1797157
Hunter Dobbelmann <hunterdobb@gmail.com>
2024-12-04 23:09:10 -0500

    clean up

--- backend/src/routes/volunteer.route.js
+++ backend/src/routes/volunteer.route.js
@@ -24,33 +24,8 @@ module.exports = router
 
 
 
-// --- NOT SURE IF WE NEED THESE --- //
+// --- NOT SURE IF WE NEED THIS --- //
 
-//     // Get profile by email
-//     route.get('/profile/:email', async (req, res) => {
-//         let email = req.params.email;
-//         if (!email) {
-//             return res.status(400).send({
-//                 error: 'Invalid parameters'
-//             });
-//         }
-//
-//         let volunteer = await userModel.findOne({
-//             Email: email
-//         }, {
-//             Password: 0 // let's not send the password :)
-//         }).exec();
-//
-//         if (!volunteer) {
-//             return res.status(404).send({
-//                 error: 'Volunteer not found'
-//             });
-//         }
-//
-//         res.send(volunteer);
-//     });
-//
-//
 //     // update availability
 //     route.post('/availability', async (req, res) => {
 //         let {availability} = req.body;
